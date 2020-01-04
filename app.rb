2a2e5327a94e5cd93ef2b363499f33d43616e446
Your Nametikurila <you@example.comkarinapopova20044002pop@gmail.com>
2020-01-04 12:47:00 +0200

    files that I do not touch

---
+++ app.rb
@@ -0,0 +1,50 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello!!!!!! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/about' do
+	
+	erb :about
+end	
+
+
+get '/contacts' do
+	erb :contacts
+end	
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@data_time = params[:data_time]
+	@barber = params[:barber]
+	@colors = params[:colors]
+
+
+  hh = {  :username => 'Введите имя',
+  		  :phone => 'Введите телефон',
+  		  :data_time => 'Введите дату и время' }
+
+		@error = hh.select {|key,_| params[key] == ""}.values.join(",")
+
+		if @error != ""
+			return erb :visit
+		end	
+
+	
+erb "Пользователь: #{@username} Время записи: #{@data_time} Связь с клиентом #{@phone}  Парикмахер: #{@barber} Выбран цвет: #{@colors} "
+
+
+end	
+
+
+
